5a77e4b9955b61259742f64edb1fc0028f5ceed7
Ruchi Gupta <gupta.ruch@husky.neu.edu>
2016-08-13 14:34:25 -0400

    latest

--- recipie.js
+++ recipie.js
@@ -1,4 +1,5 @@
 var message, nextvalue = 0;
+var starting = document.getElementById('start');
 
 function voiceDev() {
 
@@ -13,11 +14,14 @@ function voiceDev() {
   results.setAttribute('id', 'resultsDiv');
 
   var status = document.createElement('div');
+  status.setAttribute('id', 'stat');
+  status.style.marginLeft = "50px";
   status.innerHTML = 'Not Listening.';
   results.appendChild(status);
 
   var speechResult = document.createElement("div");
   speechResult.innerHTML = '';
+  speechResult.style.marginLeft = "50px";
   speechResult.className = "speech";
   results.appendChild(speechResult);
 
@@ -28,6 +32,8 @@ function voiceDev() {
   alternativesDebug.className = "alternatives";
   document.body.appendChild(alternativesDebug);
 
+
+
   if (!SpeechRecognition) {
     status.innerHTML = "No Speech Capabilities Found";
   } else {
@@ -36,13 +42,23 @@ function voiceDev() {
     recognition.interimResults = false;
     recognition.continuous = true;
     recognition.maxAlternatives = 1;
-    recognition.start();
+
+    var start = new SpeechSynthesisUtterance("Are you ready");
+    start.rate = 1.3;
+    window.speechSynthesis.speak(start);
+    start.onend = function(event) {
+      recognition.start();
+    };
+
 
     document.getElementById("disable").onclick = function() {
       document.getElementById('allowChat').src = 'chat.png';
       recognition.stop();
       status.innerHTML = 'No Longer Listening.';
-    }
+      var stop = "Thank you! Have a Great Day";
+      var stop1 = new SpeechSynthesisUtterance(stop);
+      window.speechSynthesis.speak(stop1);
+    };
 
     recognition.onnomatch = function(event) {
       console.log("Did not recognize");
@@ -59,24 +75,20 @@ function voiceDev() {
       var result = event.results[0];
       if (result[0].transcript == 'yes') {
 
-        //  var recipes = result[0].transcript;
-        //recipes = recipes.toLowerCase().trim();
-        // var commandArray = recipes.split(' ');
-        // var length = commandArray.length;
-
 
         $.ajax({
             method: "GET",
             url: "http://recipio.herokuapp.com/recipes",
             data: {
-              recipe: "pizza"
+              recipe: "omelette"
             }
           })
           .done(function(msg) {
             message = msg.steps;
             console.log(result[0].transcript);
             var utterance = new SpeechSynthesisUtterance(message[nextvalue]);
-            utterance.rate = 0.7;
+            utterance.rate = 1.3;
+            starting.innerHTML = message[nextvalue];
             window.speechSynthesis.speak(utterance);
 
             utterance.onend = function(event) {
@@ -86,35 +98,77 @@ function voiceDev() {
 
           });
       } else if (result[0].transcript.includes('next') || result[0].transcript.includes('Next')) {
-        console.log(result[0].transcript);
-        nextvalue++;
-        var utteranceNext = new SpeechSynthesisUtterance(message[nextvalue]);
-        utteranceNext.rate = 0.7;
-        window.speechSynthesis.speak(utteranceNext);
 
-        utteranceNext.onend = function(event) {
-          recognition.start();
-        };
+        if (nextvalue !== (message.length - 1)) {
+          console.log(result[0].transcript);
+          nextvalue++;
+          var utteranceNext = new SpeechSynthesisUtterance(message[nextvalue]);
+          utteranceNext.rate = 1.3;
+          starting.innerHTML = message[nextvalue];
+          window.speechSynthesis.speak(utteranceNext);
 
-      } else if (result[0].transcript === 'previous' || result[0].transcript === 'Previous') {
-        nextvalue--;
-        console.log(result[0].transcript);
-        var utterancePrevious = new SpeechSynthesisUtterance(message[nextvalue]);
-        utterancePrevious.rate = 0.7;
-        window.speechSynthesis.speak(utterancePrevious);
+          utteranceNext.onend = function(event) {
+            recognition.start();
+          };
+        } else {
+          var lastStep = new SpeechSynthesisUtterance("That was the last step");
+          lastStep.rate = 1.3;
+          starting.innerHTML = "That was the last step";
+          window.speechSynthesis.speak(lastStep);
 
-        utterancePrevious.onend = function(event) {
-          recognition.start();
-        };
+          lastStep.onend = function(event) {
+            recognition.start();
+          };
+        }
+
+
+      } else if (result[0].transcript.includes('previous') || result[0].transcript.includes('Previous')) {
+        if (nextvalue !== 0) {
+          nextvalue--;
+          console.log(result[0].transcript);
+          var utterancePrevious = new SpeechSynthesisUtterance(message[nextvalue]);
+          utterancePrevious.rate = 1.3;
+          starting.innerHTML = message[nextvalue];
+          window.speechSynthesis.speak(utterancePrevious);
+
+          utterancePrevious.onend = function(event) {
+            recognition.start();
+          };
+        } else {
+          var firstStep = new SpeechSynthesisUtterance("That was the first step");
+          firstStep.rate = 1.3;
+          starting.innerHTML = "That was the first step";
+          window.speechSynthesis.speak(firstStep);
+
+          firstStep.onend = function(event) {
+            recognition.start();
+          };
+        }
+
 
       } else if (result[0].transcript.includes('repeat') || result[0].transcript.includes('Repeat')) {
         console.log(result[0].transcript);
         var utteranceRepeat = new SpeechSynthesisUtterance(message[nextvalue]);
-        utteranceRepeat.rate = 0.7;
+        utteranceRepeat.rate = 1.3;
+        starting.innerHTML = message[nextvalue];
         window.speechSynthesis.speak(utteranceRepeat);
 
         utteranceRepeat.onend = function(event) {
           recognition.start();
+        };
+
+      } else if (result[0].transcript.includes('stop') || result[0].transcript.includes('Stop') || result[0].transcript.includes('Bye') || result[0].transcript.includes('bye')) {
+        console.log(result[0].transcript);
+        var stop = "Thank you! Have a Great Day";
+        var stop1 = new SpeechSynthesisUtterance(stop);
+        stop1.rate = 1.3;
+        starting.innerHTML = stop;
+        window.speechSynthesis.speak(stop1);
+
+        stop1.onend = function(event) {
+          $('#myModal').hide();
+          $('.modal-backdrop').hide();
+          recognition.stop();
         };
 
       } else if (!result[0].transcript.includes('repeat') || !result[0].transcript.includes('Repeat') ||
@@ -124,8 +178,14 @@ function voiceDev() {
       ) {
         console.log(result[0].transcript);
         recognition.stop();
-        console.log('Did not Recognize! Speak Again');
-        recognition.start();
+        var sorry = new SpeechSynthesisUtterance("Sorry Can you repeat that again");
+        sorry.rate = 1.3;
+        starting.innerHTML = "Sorry Can you repeat that again";
+        window.speechSynthesis.speak(sorry);
+        sorry.onend = function(event) {
+          recognition.start();
+        };
+
 
       }
 
@@ -148,6 +208,7 @@ function voiceDev() {
     };
     recognition.onend = function() {
       status.innerHTML = 'No Longer Listening.';
+      //recognition.start();
     };
 
 
